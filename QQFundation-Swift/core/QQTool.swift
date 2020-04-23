b70f5645dd820b3da5d1ca727ca2df2e5fd5afe0
qinmuqiao <1159854187@qq.com>
2020-04-23 16:20:09 +0800

    1

--- QQFundation-Swift/core/QQTool.swift
+++ QQFundation-Swift/core/QQTool.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
-class QQTool: NSObject {
-
+extension NSObject {
+    
+    /// 转换为字符串
+    /// - Parameter str: 需要转换的值
+    func relay(str:Any) -> String {
+        if let a = str as? NSNumber {
+            return a.stringValue
+        }else if  ((str as? NSNull) != nil){
+            return ""
+        }else if let a = str as? String {
+            return a.trimStringHAT()
+        }else if let a = str as? Substring {
+            return "\(a)".trimStringHAT()
+        }
+        return ""
+    }
 }
