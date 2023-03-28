82b3e8798ccbf40062854344798ee740cd04d7cf
MuYanQin <1159854187@qq.com>
2023-03-28 13:29:01 +0800

    整理文件

--- QQFundation-Swift/core/QQTool.swift
+++ QQFundation-Swift/core/QQTool.swift
@@ -25,3 +25,22 @@ extension NSObject {
         return ""
     }
 }
+extension String{
+    
+    /// 是否是数字
+    func isNumber() -> Bool {
+        let carTest = NSPredicate(format:"SELF MATCHES ^[0-9]*$")
+        return carTest.evaluate(with: self)
+    }
+    
+    /// 去除所有空格
+    func deleteAllSpace() -> String {
+        return self.replacingOccurrences(of: " ", with: "")
+    }
+    
+    
+    /// 去除字符串首位的空格
+    func trimStringHAT() -> String {
+        return self.trimmingCharacters(in: .whitespaces)
+    }
+}
