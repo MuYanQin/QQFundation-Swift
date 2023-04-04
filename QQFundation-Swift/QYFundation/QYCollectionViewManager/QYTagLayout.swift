d9fe8e54b2da79af65f6a50657b234ede9d10f89
MuYanQin <1159854187@qq.com>
2023-04-04 17:11:28 +0800

    优化一下标签布局的算法及添加注释

--- QQFundation-Swift/QYFundation/QYCollectionViewManager/QYTagLayout.swift
+++ QQFundation-Swift/QYFundation/QYCollectionViewManager/QYTagLayout.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+/// 标签流水布局
 class QYTagLayout: UICollectionViewFlowLayout {
+    var preFrame :CGRect?
+
+    /// 获取所有的布局信息
+    /// - Parameter rect: rect description
+    /// - Returns: 布局信息
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        //获取所有的布局信息 包括cell header footer
         guard let origArray = super.layoutAttributesForElements(in: rect) else { return []}
         var updateArray = Array(origArray)
         for attributes in origArray as [UICollectionViewLayoutAttributes]{
+            //空的话 就是cell 需要重新布局位置信息。但是header footer不需要
+            // nil when representedElementCategory is UICollectionElementCategoryCell
             if attributes.representedElementKind == nil{
                 let index = updateArray.firstIndex(of: attributes) ?? 0
                 updateArray[index] = layoutAttributesForItem(at: attributes.indexPath)!
@@ -22,18 +31,28 @@ class QYTagLayout: UICollectionViewFlowLayout {
     }
     
     override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        //获取到一个布局信息
         let currentAttributes = super.layoutAttributesForItem(at: indexPath)
         let sectionInset = self.sectionInset
 
         
         let isFirstItemInSection = indexPath.item == 0
+        //获取展示的宽度
         let layoutWidth = collectionView!.frame.width - sectionInset.left - sectionInset.right
         if isFirstItemInSection {
+            //如果是第一个 从 sectionInset.left 开始做坐标
             currentAttributes!.leftAlignFrame(with: sectionInset)
+            self.preFrame = currentAttributes?.frame
             return  currentAttributes
         }
+        //获取前一个item的indexpath
         let previousIndexPath = IndexPath(item: indexPath.item - 1, section: indexPath.section)
-        let previousFrame = layoutAttributesForItem(at: previousIndexPath)?.frame ?? CGRect.zero
+        var previousFrame  = CGRect.zero
+        if self.preFrame != nil {
+            previousFrame = self.preFrame!
+        }else{
+            previousFrame = layoutAttributesForItem(at: previousIndexPath)?.frame ?? CGRect.zero
+        }
         let currentFrame = currentAttributes!.frame
         let previousFrameRightPoint = previousFrame.origin.x + previousFrame.size.width
         let strecthedCurrentFrame = CGRect(x: sectionInset.left, y: currentFrame.origin.y, width: layoutWidth, height: currentFrame.size.height)
@@ -42,12 +61,14 @@ class QYTagLayout: UICollectionViewFlowLayout {
         let isFirstItemInRow = !previousFrame.intersects(strecthedCurrentFrame)
         if isFirstItemInRow {
             currentAttributes!.leftAlignFrame(with: sectionInset)
+            self.preFrame  = nil
             return  currentAttributes
         }
 
         var frame = currentAttributes!.frame
         frame.origin.x = previousFrameRightPoint + self.minimumInteritemSpacing
         currentAttributes!.frame = frame
+        self.preFrame = frame
         return  currentAttributes
     }
 }
